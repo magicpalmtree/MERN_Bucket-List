d9c9161f81a890176e7ccca42264070fe2afad12
magicpalmtree <magicpalmtree.space@gmail.com>
2017-07-11 16:07:52 -0400

    Created API

---
+++ server/controllers/auth.js
@@ -0,0 +1,47 @@
+// This file is the controller that handles the authentication when a user is signing UP or signing OUT.
+
+// Import the User model into our controller:
+var User = require('../models/user');
+
+// Define a signup function:
+exports.signup = function(req, res, next){
+	// Grab an incoming request using email & password variables:
+	var email = req.body.email;
+	var password = req.body.password;
+	
+	// Find one instance of a specific email address. 1st argument is the email object; 2nd argument is a callback function that runs asynchronously (the function will be invoked when response is received from the server):
+	User.findOne({ email: email }, function(err, existingUser){
+	// existingUser above: If there's someone with the email, the value will be null.
+
+		// Handle a search error:
+		if(err){
+			return next(err);
+		} 
+
+		// Handle existing users:
+		if(existingUser){
+			// return res.status(418).send(err);
+			return res.status(418).send("Email is in use.");
+		}
+
+		// If the user doesn't exist, save the user:
+		var user = new User({
+			email: email,
+			password: password
+		});
+
+		// Save the user's record to the database:
+		user.save(function(err){
+			if(err) { return next(err); }
+			// Respond to request indicating the user was created:
+			res.json({ success: true });
+		});
+	});
+}
+
+
+// Module 9 Correction from Ben Cook:
+// function createUserToken(user){
+//     let timestamp = new Date().getTime();
+//     return jwt.encode({sub: user.id,iat: timestamp }, config.secret);
+// }
--- server/router.js
+++ server/router.js
@@ -1,10 +1,9 @@
-module.exports = function(app){
-	// Call the express .get method on the app. 1st argument (/) is the route that's requested, which would be localhost:3000/. If the 1st parameter's fulfilled, the 2nd argument will pop, and a function will run. The function gets called w/3 arguments:
-	app.get('/', function(req, res, next){
-		res.send("HELLO HOMEPAGE");
-	});
+// Import auth.js file:
+var Auth = require('./controllers/auth');
+// Import the User model:
+var User = require('./models/user');
 
-	app.get('/signup', function(req, res, next){
-		res.send("Hey folks, thanks for signing up!");
-	});
+// When a user wants to sign up, route her to '/signup' and run the signup function:
+module.exports = function(app){
+	app.post('/signup', Auth.signup);
 }
