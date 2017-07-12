d8d82a37473076426555a83764687b914c30b758
magicpalmtree <magicpalmtree.space@gmail.com>
2017-07-12 15:29:55 -0400

    Added video and search bar components

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import { Component } from 'react';
-import NavBarHeader from './nav';
+import React, { Component } from 'react';
+import NavBarHeader from './Nav';
+// Import video.js:
+import Video from './video/video';
 
 	export default class App extends Component {
 		render() {
 			return (
 				<div>
 					<NavBarHeader />
+				{/* Add an instance of Video: */}
+					<Video />
 				</div>
 			);
 		}
